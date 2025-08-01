aac9db7de91ed5c175f8cfade85ee3445793978e
Yadunandan Katragadda <katragadda225@gmail.com>
2025-08-01 11:59:50 +0530

    Fix Vite config and Amplify build issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          chart: ['chart.js', 'react-chartjs-2'],
-        }
-      }
-    }
-  },
-  optimizeDeps: {
-    include: ['@emailjs/browser']
-  },
-  define: {
-    global: 'globalThis'
+    outDir: 'dist'
   }
 })
