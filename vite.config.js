0366ebcdd56d762c67ef908ecde8fc9fb7f11afb
Yadunandan Katragadda <katragadda225@gmail.com>
2025-08-08 11:23:35 +0530

    🔧 Switch to test Razorpay key

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ export default {
   },
   define: {
     'process.env.REACT_APP_PAYMENT_API_URL': JSON.stringify(process.env.REACT_APP_PAYMENT_API_URL || 'https://zk23bd25ccxknvkxh4bp6td4vy0fhujq.lambda-url.us-east-1.on.aws/'),
-    'process.env.REACT_APP_RAZORPAY_KEY_ID': JSON.stringify(process.env.REACT_APP_RAZORPAY_KEY_ID || 'rzp_live_rXTFpXbw0dmrLy')
+    'process.env.REACT_APP_RAZORPAY_KEY_ID': JSON.stringify(process.env.REACT_APP_RAZORPAY_KEY_ID || 'rzp_test_GwLaqT264JyMlU')
   }
 };
